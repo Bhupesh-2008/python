edc6d70994c052571884ee82e0a46de2f984364a
Bhupesh-2008 <217087566+Bhupesh-2008@users.noreply.github.com.>
2026-05-12 15:09:45 +0530

    sh

---
+++ python questions /lengthOfaStringWithoutLenFunctio.py
@@ -0,0 +1,5 @@
+s= str(input("Enter the string whose length you want to find: "))
+cnt=0
+for i in s:
+    cnt+=1
+print("Length of String",s,"is",cnt)
---
+++ python questions /usernameFromEmail.py
@@ -0,0 +1,2 @@
+email= str(input("Enter the email: "))
+print("Your username is: ",email[0:email.index('@')])
